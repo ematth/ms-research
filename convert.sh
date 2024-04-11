a80c944ed54bac0b0c200f62a7e01efecc4189c9
Evan Matthews <evanmm3@illinois.edu>
2024-04-11 16:13:33 -0500

    bach data

--- convert.sh
+++ convert.sh
@@ -1,5 +1,6 @@
 echo "converting .mid to .ly..."
-/home/evanmm3/bin/midi2ly -d $2 -o outputs/"$1"/"$1$2".ly "$1".mid
+
+/home/evanmm3/bin/midi2ly -d $2 -o outputs/"$1$2".ly "$1".mid # -t 1*1/1 
 
 echo "converting .ly to .pdf..."
-/home/evanmm3/bin/lilypond -o outputs/"$1"/"$1$2" outputs/"$1"/"$1$2".ly
+/home/evanmm3/bin/lilypond -s -fpng -dcrop="#t" -o outputs/"$1$2" outputs/"$1$2".ly #-dpng-width="595" -dpng-height="842"
