bc6fcfc21a4f352dfed883165f0684cc791e5d9d
Evan Matthews <evanmm3@illinois.edu>
2024-05-03 17:00:38 -0500

    optimal quantization value found for midi files

---
+++ quant_parse.py
@@ -0,0 +1,40 @@
+from os.path import isfile
+from mido import MidiFile, bpm2tempo, tempo2bpm
+
+
+
+def quant_parse(num: int = 1, path: str = './bach/invent/invent') -> int:
+    """Parses a lilypond file for the maximum note quantization. 
+
+    Args:
+        num (int): work number to analyze. Defaults to 1.
+        path (str, optional): path to directory of works. Defaults to './bach/invent/invent'.
+
+    Returns:
+        int: optimal quantization value
+    """
+
+    # File check
+    if not isfile(path + str(num) + '.mid'):
+        raise FileNotFoundError(f'File of path {path}, #{num} does not exist.')
+
+    max_quant = -1
+    mid_file = MidiFile(path + str(num) + '.mid')
+    numerator = None; denominator = None
+    for msg in mid_file:
+        if msg.type == 'time_signature':
+             numerator = msg.numerator
+             denominator = msg.denominator
+        elif msg.type == 'set_tempo':
+             tempo = msg.tempo/1_000_000
+             bpm = tempo2bpm(tempo, time_signature=(numerator, denominator))
+        elif msg.type in ['note_on', 'note_off']:
+            if msg.time != 0:
+                print(tempo / msg.time)
+    return bpm
+
+
+
+
+if __name__ == '__main__':
+    quant_parse(1)
