0ed1e66bb9a9960fd99dccaecae65a4cd42f2b6f
Evan Matthews <evanmm3@illinois.edu>
2024-05-03 17:10:23 -0500

    optimal quantization found!

--- quant_parse.py
+++ quant_parse.py
@@ -1,6 +1,6 @@
 from os.path import isfile
 from mido import MidiFile, bpm2tempo, tempo2bpm
-
+from math import log2
 
 
 def quant_parse(num: int = 1, path: str = './bach/invent/invent') -> int:
@@ -18,7 +18,7 @@ def quant_parse(num: int = 1, path: str = './bach/invent/invent') -> int:
     if not isfile(path + str(num) + '.mid'):
         raise FileNotFoundError(f'File of path {path}, #{num} does not exist.')
 
-    max_quant = -1
+    max_quant = 0
     mid_file = MidiFile(path + str(num) + '.mid')
     numerator = None; denominator = None
     for msg in mid_file:
@@ -27,14 +27,14 @@ def quant_parse(num: int = 1, path: str = './bach/invent/invent') -> int:
              denominator = msg.denominator
         elif msg.type == 'set_tempo':
              tempo = msg.tempo/1_000_000
-             bpm = tempo2bpm(tempo, time_signature=(numerator, denominator))
-        elif msg.type in ['note_on', 'note_off']:
-            if msg.time != 0:
-                print(tempo / msg.time)
-    return bpm
-
-
+            #  bpm = tempo2bpm(tempo, time_signature=(numerator, denominator))
+        elif msg.type in ['note_on', 'note_off'] and msg.time != 0:
+            quant = int(tempo / msg.time)
+            if quant & (quant - 1) == 0 and quant != 0 and quant > max_quant:
+                max_quant = quant
+    return max_quant * numerator
 
 
 if __name__ == '__main__':
-    quant_parse(1)
+    q = quant_parse(1)
+    print(q)
