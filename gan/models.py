66a228ad39a6eaf7a6f112f43d057f4898807849
Evan Matthews <evanmm3@illinois.edu>
2024-06-10 15:15:43 -0500

    hifi-gan framework

---
+++ gan/models.py
@@ -0,0 +1,29 @@
+import torch
+import torch.nn.functional as F
+import torch.nn as nn
+
+# TODO: Generator akin to HiFi-GAN for music.
+class Generator(torch.nn.Module):
+    def __init__():
+        pass
+
+    def forward():
+        pass
+
+
+# TODO: Discriminator akin to HiFi-GAN for music.
+class Discriminator(torch.nn.Module):
+    def __init__():
+        pass
+
+    def forward():
+        pass
+
+def feature_loss():
+    pass
+
+def generator_loss():
+    pass
+
+def discriminator_loss():
+    pass
