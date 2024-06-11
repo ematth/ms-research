67e995300b5601202fead11832db8c42f0538fe5
Evan Matthews <evanmm3@illinois.edu>
2024-06-10 19:44:26 -0500

    generator

--- gan/models.py
+++ gan/models.py
@@ -108,28 +108,79 @@ class MelDataset(nn.Module):
         return len(self.audio)
 
 
-class ResnetBlock(nn.Module):
-    def __init__(self, dim, dilation=1):
-        super().__init__()
-        self.block = nn.Sequential(
-            nn.LeakyReLU(negative_slope=0.2),
-            nn.ReflectionPad1d(padding=dilation),
-            weight_norm(nn.Conv1d(dim, dim, kernel_size=3, dilation=dilation)),
-            nn.LeakyReLU(negative_slope=0.2),
-            weight_norm(nn.Conv1d(dim, dim, kernel_size=1))
-        )
-        self.shortcut = weight_norm(nn.Conv1d(dim, dim, kernel_size=1))
+def padding(kernel_size, dilation=1) -> int:
+    return (kernel_size * dilation - dilation) // 2
 
-    def forward(self, x): return self.shortcut(x) + self.block(x)
+LEAKY_SLOPE = 0.1
+
+class ResnetBlock1(nn.Module):
+    def __init__(self, channels, kernel_size=3, dilation=(1, 3, 5)):
+        super(ResnetBlock1, self).__init__()
+        self.conv1 = nn.ModuleList([
+            weight_norm(nn.Conv1d(channels, channels, kernel_size, 1, dilation=dilation[0], padding=nn.get_padding(kernel_size, dilation[0]))),
+            weight_norm(nn.Conv1d(channels, channels, kernel_size, 1, dilation=dilation[1], padding=nn.get_padding(kernel_size, dilation[1]))),
+            weight_norm(nn.Conv1d(channels, channels, kernel_size, 1, dilation=dilation[2], padding=nn.get_padding(kernel_size, dilation[2])))
+        ])
+        #self.conv1.apply(init_weights) mean=0, std=0.01
+
+        self.conv2 = nn.ModuleList([
+            weight_norm(nn.Conv1d(channels, channels, kernel_size, 1, dilation=1, padding=nn.get_padding(kernel_size, 1))),
+            weight_norm(nn.Conv1d(channels, channels, kernel_size, 1, dilation=1, padding=nn.get_padding(kernel_size, 1))),
+            weight_norm(nn.Conv1d(channels, channels, kernel_size, 1, dilation=1, padding=nn.get_padding(kernel_size, 1)))
+        ])
+        #self.conv2.apply(init_weights) mean=0, std=0.01
+
+    def forward(self, x):
+        for c1, c2 in zip(self.conv1, self.conv2):
+            xt = F.leaky_relu(x, LEAKY_SLOPE)
+            xt = c1(xt)
+            xt = F.leaky_relu(x, LEAKY_SLOPE) 
+            xt = c2(xt)
+            x += xt
+        return x
 
 
 # TODO: Generator akin to Mel-GAN for music.
 class Generator(nn.Module):
-    def __init__():
-        pass
+    def __init__(self, h):
+        super(Generator, self).__init__()
+        self.h = h
+        self.num_kernels = len(h.resblock_kernel_sizes)
+        self.num_upsamples = len(h.upsample_rates)
+        self.conv_pre = weight_norm(nn.Conv1d(80, h.upsample_initial_channel, 7, 1, padding=3))
+        resblock = ResnetBlock1
+        
+        self.ups = nn.ModuleList()
+        for i, (u, k) in enumerate(zip(h.upsample_rates, h.upsample_kernel_sizes)):
+            self.ups.append(weight_norm(
+                nn.ConvTranspose1d(h.upsample_initial_channel//(2**i), h.upsmaple_initial_channel//(2**(i+1)), k, u, padding=(k-u)//2)))
+            
+        self.resblocks = nn.ModuleList()
+        for i in range(len(self.ups)):
+            ch = h.upsample_initial_channel//(2**(i+1))
+            for (k, d) in zip(h.resblock_kernel_sizes, h.resblock_dilation_sizes):
+                self.resblocks.append(resblock(h, ch, k, d))
 
-    def forward():
-        pass
+        self.conv_post = weight_norm(nn.Conv1d(ch, 1, 7, 1, padding=3))
+        #self.ups.apply(init_weights)
+        #self.conv_post.apply(init_weights)   
+
+    def forward(self, x):
+        x = self.conv_pre(x)
+        for i in range(self.num_upsamples):
+            x = F.leaky_relu(x, LEAKY_SLOPE)
+            x = self.ups[i](x)
+            xs = None
+            for j in range(self.num_kernels):
+                if xs is None:
+                    xs = self.resblocks[i*self.num_kernels+j](x)
+                else:
+                    xs += self.resblocks[i*self.num_kernels+j](x)
+            x = xs / self.num_kernels
+        x = F.leaky_relu(x)
+        x = self.conv_post(x)
+        x = torch.tanh(x)
+        return x
 
 
 # TODO: Discriminator akin to Mel-GAN for music.
